99c452e4956ff4566e686bc2b01b4f654e3bdca5
Nikolay Shayahmetov <shayahmetov@gmail.com>
2017-03-21 10:58:04 +0500

    Metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -12,6 +12,7 @@ use common\widgets\Alert;
 use common\models\Type;
 
 AppAsset::register($this);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -86,6 +87,56 @@ AppAsset::register($this);
         <div class="col-md-6">
             <p class="text-muted"> Техническая поддержка <?= Html::a('support@prodamokt.ru', 'mailto:support@prodamokt.ru')?></p>
             <p class="text-muted"> По вопросам релкамы и сотрудничества <?= Html::a('reklama@prodamokt.ru', 'mailto:reklama@prodamokt.ru')?></p>
+        </div>
+        <div class="col-md-4">
+            <p class="pull-right">
+                <?php
+                if (YII_ENV_PROD)
+                {
+                ?>
+                <!-- Yandex.Metrika informer -->
+                <a href="https://metrika.yandex.ru/stat/?id=43608799&amp;from=informer"
+                   target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/43608799/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                                       style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="43608799" data-lang="ru" /></a>
+                <!-- /Yandex.Metrika informer -->
+
+                <!-- Yandex.Metrika counter -->
+                <script type="text/javascript">
+                    (function (d, w, c) {
+                        (w[c] = w[c] || []).push(function() {
+                            try {
+                                w.yaCounter43608799 = new Ya.Metrika({
+                                    id:43608799,
+                                    clickmap:true,
+                                    trackLinks:true,
+                                    accurateTrackBounce:true,
+                                    webvisor:true
+                                });
+                            } catch(e) { }
+                        });
+
+                        var n = d.getElementsByTagName("script")[0],
+                            s = d.createElement("script"),
+                            f = function () { n.parentNode.insertBefore(s, n); };
+                        s.type = "text/javascript";
+                        s.async = true;
+                        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+                        if (w.opera == "[object Opera]") {
+                            d.addEventListener("DOMContentLoaded", f, false);
+                        } else { f(); }
+                    })(document, window, "yandex_metrika_callbacks");
+                </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/43608799" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
+            <?php
+            }
+            else
+            {
+                echo Html::img('http://fakeimg.pl/88x31/', ['style' => 'width:88px; height:31px; border:0;']);
+            }
+            ?>
+            </p>
         </div>
 
 
